6939dbef3a492d6e6edc591a451b27a5f28b0a4a
Martin Six <msix@hopper.com>
2025-09-03 21:15:46 +1000

    Revert "Add Capacitor mobile wrapper project"
    This reverts commit 276ad8ef6b13b12b8f0641a95d26b524aabb80ef.

--- nimble-mobile/dist/_next/static/chunks/app/layout-e91185d37e00c551.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1290:()=>{},3766:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1290,23)),Promise.resolve().then(n.bind(n,8720))}},e=>{e.O(0,[741,720,441,255,358],()=>e(e.s=3766)),_N_E=e.O()}]);
